5c7f816fb1a2fa756f374de37add3533d3028f29
Roman Kavyrshin <rom.kawirshin@yandex.ru>
2021-07-07 03:53:33 +0300

    Исправлен CompositeViewRenderer.kt

--- app/src/main/java/ru/kavyrshinr/treerecycler/viewrenderer/composite/CompositeViewRenderer.kt
+++ app/src/main/java/ru/kavyrshinr/treerecycler/viewrenderer/composite/CompositeViewRenderer.kt
@@ -1,10 +1,9 @@
 package ru.kavyrshinr.treerecycler.viewrenderer.composite
 
 import androidx.recyclerview.widget.RecyclerView
-import ru.kavyrshinr.treerecycler.viewrenderer.ItemModel
 import ru.kavyrshinr.treerecycler.viewrenderer.ViewRenderer
 
-abstract class CompositeViewRenderer<M : ItemModel, VH : RecyclerView.ViewHolder>
+abstract class CompositeViewRenderer<M : CompositeItemModel, VH : RecyclerView.ViewHolder>
     : ViewRenderer<M, VH>() {
 
     abstract var expandListener: (Int) -> Unit
